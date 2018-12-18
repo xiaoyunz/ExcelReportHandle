f7e861c34adcd8e8cb7601076c757b51c56048a3
xiaoyunz <416912931@qq.com>
2018-12-18 18:56:43 +0800

    Excel导入导出工具

--- src/main/java/com/reports/exports/xmlhandle/ReadReportsXml.java
+++ src/main/java/com/reports/exports/xmlhandle/ReadReportsXml.java
@@ -6,6 +6,7 @@ import java.io.InputStreamReader;
 import java.nio.charset.Charset;
 
 import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.io.xml.DomDriver;
 
 /**
  * 读取xml文件，解析为对象
@@ -16,8 +17,8 @@ public class ReadReportsXml {
 
 		if (filePath != null && filePath.trim().length() > 0) {
 
-			XStream xm = new XStream();
-			xm.processAnnotations(Reports.class);
+			XStream xm = new XStream(new DomDriver());    //注意：不是new Xstream(); 否则报错：java.lang.NoClassDefFoundError: org/xmlpull/v1/XmlPullParserFactory
+			xm.processAnnotations(Reports.class);         //如果是用注解的方式，这句不能少
 
 			Reports reports = (Reports) xm.fromXML(new InputStreamReader(new FileInputStream(filePath),Charset.forName("UTF-8")));
 			return reports;
--- src/main/java/com/reports/imports/xmlhandle/ReadImportsXml.java
+++ src/main/java/com/reports/imports/xmlhandle/ReadImportsXml.java
@@ -4,7 +4,10 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStreamReader;
 import java.nio.charset.Charset;
+
+import com.reports.exports.xmlhandle.Reports;
 import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.io.xml.DomDriver;
 
 /**
  * 读取xml文件，解析为对象
@@ -15,8 +18,8 @@ public class ReadImportsXml {
 
 		if (filePath != null && filePath.trim().length() > 0) {
 
-			XStream xm = new XStream();
-			xm.processAnnotations(Imports.class);
+			XStream xm = new XStream(new DomDriver());    //注意：不是new Xstream(); 否则报错：java.lang.NoClassDefFoundError: org/xmlpull/v1/XmlPullParserFactory
+			xm.processAnnotations(Imports.class);         //如果是用注解的方式，这句不能少
 
 			Imports imports = (Imports) xm.fromXML(new InputStreamReader(new FileInputStream(filePath),Charset.forName("UTF-8")));
 			return imports;
